a93a424b905887df7d1a8b92615d4f2b9cfbd471
Joey <usjoeylou@gmail.com>
2020-12-07 00:12:01 -0600

    add readme, tkinter example

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# python-dev
-Development practice projects in Python
+# Python Dev Poractice
+Development practice projects in Python\
+Referencing Udemy course *100 days of Code* by Angela 
+
+## Day 18
+- check out turtle package
+- simple drawing app using turtle
+
+## Day 19
+- practice class inheritance
+- 2D shooting game
+
+## Day 27
+- check out Tkinter, practice functional arguments
+- lb to kg coverter
+
---
+++ day27/weight_converter.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from tkinter import ttk
+
+class WeightConverter:
+
+    def __init__(self, root):
+
+        root.title("Pound to Kilograms")
+
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+       
+       # create input and output cells
+        self.pound = StringVar()
+        pound_entry = ttk.Entry(mainframe, width=7, textvariable=self.pound)
+        pound_entry.grid(column=1, row=1, sticky=(W, E))
+        self.kilogram = StringVar()
+
+        ttk.Label(mainframe, text="lb").grid(column=2, row=1, sticky=W)
+        ttk.Label(mainframe, text="=").grid(column=3, row=1, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.kilogram).grid(column=4, row=1, sticky=(W, E))
+        ttk.Label(mainframe, text="kg").grid(column=5, row=1, sticky=E)
+        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=4, row=2, sticky=(W, E))
+
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+
+        pound_entry.focus()
+        root.bind("<Return>", self.calculate)
+        
+    def calculate(self, *args):
+        try:
+            value = float(self.pound.get())
+            self.kilogram.set(round(value * 0.45359237, 2))
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    root = Tk()
+    WeightConverter(root)
+    root.mainloop()
